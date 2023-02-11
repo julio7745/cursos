6586be748926e2b5ee15804c06ddd97136dbeb37
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-02-11 10:19:50 -0300

    024 -- 025 + modelo html

--- JavaScript/udemy/aula_024/index.js
+++ JavaScript/udemy/aula_024/index.js
@@ -1,1 +1,8 @@
 // if, else if, e else 
+let hr = 18
+let cumprimento = (hr) => {
+    if (hr <= 12){return 'Bom dia';} else
+    if (hr <= 17){return 'Bom tarde';} else
+    { return 'Boa noite';}
+}
+console.log(cumprimento(hr));
