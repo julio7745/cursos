8c7827dc3c2d1954aaf4589987ae9d6662c960c6
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-02-13 14:07:53 -0300

    035

---
+++ JavaScript/udemy/aula_034/index.js
@@ -0,0 +1,16 @@
+//for 
+console.log('Linha 0');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+console.log('');
+
+for (let i = 0; i <= 5; i++){
+    console.log(`Linha ${i}`);
+}console.log('');
+
+for (let i = 0; i <= 5; i++){
+    console.log(`${i} é ${i % 2 == 0 ? 'par' : 'impar'}`);
+}console.log('');
