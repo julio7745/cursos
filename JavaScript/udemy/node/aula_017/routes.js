7ebd176622830c85b5ccab613c67fd17d1f456e9
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-04-18 15:05:47 -0300

    -017

--- JavaScript/udemy/node/aula_017/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router();
-
-const homeController = require('./src/controllers/homeController.js')
-
-route.get('/', homeController.home)
-
-module.exports = route;
--- JavaScript/udemy/node/aula_017/src/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-exports.home = (req, res) => {
-    res.render('index.ejs', {
-        titulo: 'Esse é o titulo da <span style="color:red">pagina 1</span>',
-        numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    });
-}
