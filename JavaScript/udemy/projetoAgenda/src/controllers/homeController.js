14bceacef89ff82e6bea83e33488d7a344e8841f
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-04-24 16:56:39 -0300

    inicio dos guias

---
+++ JavaScript/udemy/projetoAgenda/src/controllers/homeController.js
@@ -0,0 +1,6 @@
+exports.home = (req, res) => {
+    res.render('index.ejs', {
+        titulo: 'Esse é o titulo da <span style="color:red">pagina 1</span>',
+        numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    });
+}
